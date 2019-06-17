73277c2519d84270cf64254cb9667ad363cc30a6
contedenis <contedenis@outlook.com>
2019-06-17 18:43:04 -0300

    feat/classnames-packages (#8)

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import cn from 'classnames';
 import PropTypes from 'prop-types';
 
-function Title({ children }) {
+import './styles.scss';
+
+function Title({ children, className }) {
   return (
-    <h3 className="title">{children}</h3>
+    <h3 className={cn('title', className)}>{children}</h3>
   );
 }
 
+Title.defaultProps = {
+  className: '',
+};
+
 Title.propTypes = {
   children: PropTypes.string.isRequired,
+  className: PropTypes.string,
 };
 
 export default Title;
